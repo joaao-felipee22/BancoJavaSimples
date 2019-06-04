94956a0064e3c6ab1d0be696652b9b4d1cf6cc25
Joao Felipe <deidara2enpai@gmail.com>
2019-06-04 09:44:30 -0300

    Ex Banco Scanner

---
+++ Banco Santander/src/Cliente.java
@@ -0,0 +1,77 @@
+public abstract class Cliente {
+    private int nRG = 485;
+    private String titular;
+
+    //criamos um construtor e todos os get e set dos valores
+    public Cliente(String titular) {
+        this.titular = titular;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public int getnRG() {
+        return nRG;
+    }
+
+    public void setnRG(int nRG) {
+        this.nRG = nRG;
+    }
+
+
+    public static class Conta extends Cliente{
+
+        double saldo;
+
+        //utilizo a super para pegar o contrutor da classe abstrata
+        public Conta(String titular) {
+            super(titular);
+        }
+
+        //Metodos da classe Conta
+
+        public void DadosBancarios(){ //printa todos os dados do titular
+            System.out.println(" ");
+            System.out.println("-- Dados Bancarios --");
+            System.out.println("- Titular = " + this.getTitular());
+            System.out.println("- RG = " + this.getnRG());
+            System.out.println("- Saldo = " + saldo);
+            System.out.println("-- Fim dos Dados Bancarios -- ");
+            System.out.println(" ");
+        }
+
+
+
+        public void Depositar(double pValor){ // apos digitar o valor add ele a os dados bacarios
+            System.out.println(" ");
+            System.out.println("-- Realizando Deposito --");
+            System.out.println("Saldo Anterior = " + this.saldo);
+
+            this.saldo += pValor; //this.saldo = this.saldo + pValor; soma o saldo com o valor passado
+
+            System.out.println("- Saldo Atual = " + this.saldo);
+            System.out.println("-- Fim do Deposito --");
+            System.out.println(" ");
+        }
+
+        public void Saque(double pValor){ // apos digitar um valor se esse valor for > ou = saldo, ele retira o valor dos dados bancarios
+            System.out.println(" ");
+            System.out.println("-- Realizando Saque --");
+            System.out.println("- Saldo Anterior = " + this.saldo);
+
+            if(pValor <= this.saldo) {// Se tiver um saldo menor ou igual a o Saque, realizara o Saque
+                this.saldo -= pValor;
+                System.out.println("-- Saldo Atual = " + this.saldo);
+            } else {// Caso nao tenha um saque maior que o saldo
+                System.out.println("--- Saldo Insuficiente para o Saque ---");
+            }
+            System.out.println("-- Fim do Saque --");
+            System.out.println(" ");
+        }
+    }
+}
---
+++ Banco Santander/src/Maquina.java
@@ -0,0 +1,45 @@
+
+import java.util.Scanner;
+
+public class Maquina {
+    public static void main(String[] args) {
+        // cria um objeto Scanner, e um objeto para capturar o que o sera digitado no terminal
+        Scanner in = new Scanner(System.in);
+        String aux;
+        int opcao;
+        double valor;
+
+        //inicia o programa pedindo o nome do titular
+        System.out.println("Digite o nome do Titular: ");
+        aux = in.nextLine();
+
+        //Crio um objeto da classe Conta, como a classe Conta esta dentro da classe abstrata Cliente
+        Cliente.Conta c1 = new Cliente.Conta(aux);
+
+
+        //Apos ter um objeto da classe conta e possivel utilizar os metodos que criamos
+        do{ // cria um menu com 4 opcoes possiveis
+            System.out.println("--- MENU ---");
+            System.out.println("1) Dados Bancarios");
+            System.out.println("2) Deposito");
+            System.out.println("3) Saque");
+            System.out.println("0) Finalizar");
+
+            System.out.print("Opcao: ");
+            opcao = in.nextInt();
+
+            if(opcao == 1){ // se a primeira opcao for selecionada chama o metodo DadosBancarios
+                c1.DadosBancarios();
+            }else if(opcao == 2){// se a segunda opcao for selecionada solicita um valor e depois chama o metodo Depositar
+                System.out.println("Digite o valor do Deposito: ");
+                valor = in.nextDouble();
+                c1.Depositar(valor);
+            }else if(opcao == 3){ // se a terceira opcao dor selecionada solicita um valor e chama o metodo Saque
+                System.out.println("Digite o valor do Saque: ");
+                valor = in.nextDouble();
+                c1.Saque(valor);
+            }
+
+        }while (opcao != 0);
+    }
+}
